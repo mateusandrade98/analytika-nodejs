01e5d55d2890992918d3e9c3c4bead1224cfba99
Mateus Andrade <38530760+joandesonandrade@users.noreply.github.com>
2022-05-02 22:15:57 -0300

    insert only

--- app-mongodb.js
+++ app-mongodb.js
@@ -14,25 +14,8 @@ class MDB{
             await client.connect();
             const database = client.db(env.MONGODB_DB);
             const table = database.collection(env.MONGODB_TABLE);
-            const filter = {
-                "id_link": data["id_link"],
-                "fp": data["fp"],
-                "ip": data["ip"]
-            }
-            const get = await table.findOne(filter);
             data["_id"] = data["identifier"];
-            if(!get){
-                //insert
-                await table.insertOne(data);
-                //await client.close();
-            }else{
-                //update
-                await table.updateOne(
-                    {_id: get["_id"]},
-                    {$set:data}
-                );
-                //await client.close();
-            }
+            await table.insertOne(data);
           } finally {
             
           }
