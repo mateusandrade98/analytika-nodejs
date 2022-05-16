e7e3bf5cbd9a135accffe47ae2d0539ff78766fc
Mateus Andrade <38530760+joandesonandrade@users.noreply.github.com>
2022-05-16 15:38:08 -0300

    remove quit

--- app-ws.js
+++ app-ws.js
@@ -49,7 +49,6 @@ async function sendMetricsToServer(key){
         metric.hashIdentifier = md5(ws_data.identifier);
         app_mongodb.set(metric);
     }
-    app_redis.quit();
 }
 
 function onError(ws, err) {
